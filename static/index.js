d99b01818fcbec576134b37d87674c1c7969c39e
deboravpereira <100171504+deboravpereira@users.noreply.github.com>
2023-11-20 14:00:27 -0300

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,33 +1,41 @@
 //Crie a variável de data
-
+var date = new Date()
+let display_date= "Data: " + date.toLocaleDateString('pt-BR', {weekday: 'short', year: 'numeric', month: 'short', day: 'numeric'})
 
 //Carregue o DOM HTML
+$(document).ready()
+
 
 //Defina a variável para armazenar a emoção prevista
-
+let predicted_emotion;
 
 //HTML-->JavaScript--->Flask
 //Flask--->JavaScript--->HTML
 
 //seletor jQuery e ação de clique
-
 $(function () {
     $("#predict_button").click(function () {
-        //chamada AJAX
+        //Guarde informação digitada
 
+        //Chamada ajax
         $.ajax({
+            type: '',
+            url: " ",
+            data:  ,
+            dataType: " ",
+            contentType: ' ',
+            success: function (result) {
+               
+               // Resultado recebido do Flask ----->JavaScript
+                
+               // Exibir resultado usando JavaScript----->HTML
             
-            
-              {
-                
-                // Resultado recebido do Flask ----->JavaScript
-                
-                // Exibir resultado usando JavaScript----->HTML
-                
             },
-            //Função de erro
-            
+            error: function (result) {
+                
+            }
         });
     });
 })
+
 
